47436a067f58a47ae1d1a1997b9206bd5d4ac099
Sarai <anna.sarai.rosenberg@gmail.com>
2018-02-06 18:20:37 -0800

    create methods to add stars and watchers

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import secrets
 from model import (Repo, User, Follower,
                    Stargazer, Watcher, Contributor,
                    Topic, RepoTopic,
-                   db, connect_to_db)
+                   db, connect_to_db, db_uri)
 
 token = secrets.personal_access_token
 client_id = secrets.client_id
@@ -23,8 +23,7 @@ def add_repo(repo):
 
     owner = repo.owner
     owner_id = owner.id
-    break
-    print("Must create User for owner before commiting Repo to db.")
+    # Must create User for owner before commiting Repo to db.
     add_user(owner)
     
     this_repo = Repo(repo_id=repo.id,
@@ -41,10 +40,10 @@ def update_repo(this_repo, new_repo):
 
     delta = now.timestamp() - this_repo.updated_at.timestamp()
     if delta/60/60/24/7 < 1:
-        print("Repo is up to date.")
+        print("Repo {} is up to date.".format(this_repo.name))
         return
 
-    print("Updating old repo data.")
+    print("Updating old repo data for {}.".format(new_repo.name))
 
     this_repo.name = new_repo.name
     this_repo.description = new_repo.description
@@ -58,7 +57,7 @@ def is_repo_in_db(repo):
 
     this_repo = Repo.query.filter_by(repo_id=repo.id).first()
     if this_repo:
-        print("Repo {} in db; updating.".format(repo.id))
+        print("Repo {} in db; updating.".format(repo.name))
         update_repo(this_repo, repo)
         return True
     return False
@@ -83,10 +82,10 @@ def update_user(this_user, new_user):
 
     delta = now.timestamp() - this_user.updated_at.timestamp()
     if delta/60/60/24/7 < 1:
-        print("User is up to date.")
+        print("User {} is up to date.".format(this_user.login))
         return
 
-    print("Updating old user data.")
+    print("Updating old user data for {}.".format(new_user.login))
 
     this_user.name = new_user.name
     this_user.login = new_user.login
@@ -100,7 +99,36 @@ def is_user_in_db(user):
 
     this_user = User.query.filter_by(user_id=user.id).first()
     if this_user:
-        print("User {} in db; updating.".format(user.id))
+        print("User {} in db; updating.".format(user.login))
         update_user(this_user, user)
         return True
     return False
+
+
+def add_stars(repo):
+    """Add all stargazers of repo to db."""
+    stars = repo.get_stargazers()
+
+    for star in stars:
+        add_user(star)
+
+
+def add_watchers(repo):
+    """Add all watchers of repo to db."""
+    watchers = repo.get_watchers()
+
+    for watcher in watchers:
+        add_user(watcher)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app.
+    from flask import Flask
+
+    app = Flask(__name__)
+
+    connect_to_db(app)
+    print("Connected to DB {}.".format(db_uri))
