156c81064a00f40cf911b2e2ba7e2ec89e2af139
Sarai <anna.sarai.rosenberg@gmail.com>
2018-02-06 17:51:10 -0800

    add methods to update user

--- utils.py
+++ utils.py
@@ -58,7 +58,7 @@ def is_repo_in_db(repo):
 
     this_repo = Repo.query.filter_by(repo_id=repo.id).first()
     if this_repo:
-        print("Repo {} in db; updating.".format(repo_id))
+        print("Repo {} in db; updating.".format(repo.id))
         update_repo(this_repo, repo)
         return True
     return False
@@ -73,3 +73,31 @@ def add_user(user):
                      created_at=user.created_at)
     db.session.add(this_user)
     db.session.commit()
+
+
+def update_user(this_user, new_user):
+    """Update user if it hasn't been updated in more than 7 days."""
+
+    delta = now.timestamp() - this_user.updated_at.timestamp()
+    if delta/60/60/24/7 < 1:
+        print("User is up to date.")
+        return
+
+    print("Updating old user data.")
+
+    this_user.name = new_user.name
+    this_user.login = new_user.login
+
+    db.session.add(this_user)
+    db.session.commit()
+
+
+def is_user_in_db(user):
+    """Check db for user, and update xor return false."""
+
+    this_user = User.query.filter_by(user_id=user.id).first()
+    if this_user:
+        print("User {} in db; updating.".format(user.id))
+        update_repo(this_user, user)
+        return True
+    return False
