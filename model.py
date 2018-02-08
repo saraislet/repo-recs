abcb8f5f48ef55400e0cdffe4c2991b2dfac968f
Sarai <anna.sarai.rosenberg@gmail.com>
2018-02-07 16:34:25 -0800

    add create_all to connect_to_db

--- model.py
+++ model.py
@@ -251,6 +251,7 @@ def connect_to_db(app, uri=db_uri):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
+    db.create_all()
 
 
 if __name__ == "__main__":
