d4baec708f0c03d219d6b18e552290218cd9dfe4
Sarai <anna.sarai.rosenberg@gmail.com>
2018-02-20 17:48:46 -0800

    write buildLang; reset indent

--- static/repo.jsx
+++ static/repo.jsx
@@ -7,64 +7,69 @@ class Repo extends React.Component {
     //         .then( (data) => showRepoRecs(data));
     // }
 
-    render() {
-        repos = this.props.repos
-        let listRepos = myData.map( (repo) => buildRepo(repo) );
-        return (
-            <div key={ repo.name } className="w3-card-4 w3-margin repo">
-              <header>
-                <h2 className="w3-container w3-blue-grey">{ repo.name }</h2>
-              </header>
-              <div className="w3-container">
-                <p>{ repo.description }</p>
-                <ul>
+  render() {
+    repos = this.props.repos
+    let listRepos = myData.map( (repo) => buildRepo(repo) );
+    return (
+      <div key={ repo.name } className="w3-card-4 w3-margin repo">
+        <header>
+          <h2 className="w3-container w3-blue-grey">{ repo.name }</h2>
+        </header>
+        <div className="w3-container">
+          <p>{ repo.description }</p>
+          <ul>
 
-                  <li>
-                    lang.language.language_name : lang.language_bytes
-                  </li>
+            <li>
+              lang.language.language_name : lang.language_bytes
+            </li>
 
-                </ul>
-              </div>
-            </div>
-        )
-    }
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 class RepoList extends React.Component {
+  /// getRepoRecs() {
+  //     let user_id = document.getElementById("get-repo-recs").getAttribute("user-id");
+  //     fetch("/get_repo_recs?user_id=" + user_id, showRepoRecs)
+  //         .then( (response) => response.json() )
+  //         .then( (data) => showRepoRecs(data));
+  // }
 
-    /// getRepoRecs() {
-    //     let user_id = document.getElementById("get-repo-recs").getAttribute("user-id");
-    //     fetch("/get_repo_recs?user_id=" + user_id, showRepoRecs)
-    //         .then( (response) => response.json() )
-    //         .then( (data) => showRepoRecs(data));
-    // }
-
-    render() {
-        let repos = this.props.repos
-        let listRepos = myData.map( (repo) => buildRepo(repo) );
-        return (
-            <span>{ listRepos }</span>
-        )
-    }
+  render() {
+    let repos = this.props.repos
+    let listRepos = repos.map( (repo) => buildRepo(repo) );
+    return (
+      <span>{ listRepos }</span>
+    )
+  }
 }
 
 function buildRepo(repo) {
-    return (
-        <div key={ repo.repo_id } className="w3-card-4 w3-margin repo">
-          <header className="w3-container w3-blue-grey">
-            <h3 className="w3-container w3-blue-grey">{ repo.name }</h3>
-          </header>
-          <div className="w3-container">
-            <p>{ repo.description }</p>
-            <ul>
+  let listLangs = null;
+  if ( repo.langs.length > 0 ) {
+    let languages = repo.langs.map( (lang) => buildLang(lang) );
+    listLangs = (<ul>{ languages.slice(0,3) }</ul>);
+  }
 
-              <li>
-                lang.language.language_name : lang.language_bytes
-              </li>
+  return (
+    <div key={ repo.repo_id } className="w3-card-4 w3-margin repo">
+      <header className="w3-container w3-blue-grey">
+        <h3 className="w3-container w3-blue-grey">{ repo.name }</h3>
+      </header>
+      <div className="w3-container">
+        <p>{ repo.description }</p>
+        { listLangs }
+      </div>
+    </div>
+  )
+}
 
-            </ul>
-          </div>
-        </div>
-    )
+function buildLang(lang) {
+  return (
+    <li key={ lang.language_id }>{ lang.language_name } : { lang.language_bytes }</li>
+  )
 }
 
