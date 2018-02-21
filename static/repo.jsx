480f5377be0911d7f2510f0e57263a15c99068f2
Sarai <anna.sarai.rosenberg@gmail.com>
2018-02-20 19:05:26 -0800

    clean old code

--- static/repo.jsx
+++ static/repo.jsx
@@ -1,43 +1,4 @@
-class Repo extends React.Component {
-
-    // getRepoRecs() {
-    //     let user_id = document.getElementById("get-repo-recs").getAttribute("user-id");
-    //     fetch("/get_repo_recs?user_id=" + user_id, showRepoRecs)
-    //         .then( (response) => response.json() )
-    //         .then( (data) => showRepoRecs(data));
-    // }
-
-  render() {
-    repos = this.props.repos
-    let listRepos = myData.map( (repo) => buildRepo(repo) );
-    return (
-      <div key={ repo.name } className="w3-card-4 w3-margin repo">
-        <header>
-          <h2 className="w3-container w3-blue-grey">{ repo.name }</h2>
-        </header>
-        <div className="w3-container">
-          <p>{ repo.description }</p>
-          <ul>
-
-            <li>
-              lang.language.language_name : lang.language_bytes
-            </li>
-
-          </ul>
-        </div>
-      </div>
-    )
-  }
-}
-
 class RepoList extends React.Component {
-  /// getRepoRecs() {
-  //     let user_id = document.getElementById("get-repo-recs").getAttribute("user-id");
-  //     fetch("/get_repo_recs?user_id=" + user_id, showRepoRecs)
-  //         .then( (response) => response.json() )
-  //         .then( (data) => showRepoRecs(data));
-  // }
-
   render() {
     let repos = this.props.repos
     let listRepos = repos.map( (repo) => buildRepo(repo) );
@@ -57,7 +18,15 @@ function buildRepo(repo) {
   return (
     <div key={ repo.repo_id } className="w3-card-4 w3-margin repo">
       <header className="w3-container w3-blue-grey">
-        <h3 className="w3-container w3-blue-grey">{ repo.name }</h3>
+        <h3 className="w3-container w3-blue-grey">
+          <a class="owner" href={"https://www.github.com/" + repo.owner_login }>
+            @{ repo.owner_login } 
+          </a>
+          &nbsp;/&nbsp;
+          <b><a href={"" + repo.url }>
+            { repo.name }
+          </a></b>
+        </h3>
       </header>
       <div className="w3-container">
         <p>{ repo.description }</p>
@@ -72,4 +41,3 @@ function buildLang(lang) {
     <li key={ lang.language_id }>{ lang.language_name } : { lang.language_bytes }</li>
   )
 }
-
