c54883d228c1a0af00a39aa045316306ef9a52cf
Sarai <anna.sarai.rosenberg@gmail.com>
2018-02-26 14:55:17 -0800

    replace old recs route with react route

--- server.py
+++ server.py
@@ -39,6 +39,9 @@ def get_user():
     user_id = request.args.get("user_id")
     login = request.args.get("login")
 
+    if user_id:
+      user_id = int(user_id)
+
     return get_user_profile(user_id=user_id, login=login)
 
 
@@ -126,46 +129,6 @@ def auth():
 
     flash("Successfully authenticated {} with Github!".format(user.login))
     return redirect("/")
-
-
-@app.route("/recs", methods=['GET'])
-def get_repo_recs():
-    if "user_id" not in session:
-        return redirect("/")
-
-    limit = int(request.args.get("count", config.DEFAULT_COUNT))
-    offset = limit * (-1 + int(request.args.get("page", 1)))
-    login = request.args.get("login")
-    user_id = request.args.get("user_id")
-    if user_id:
-        user_id = int(user_id)
-
-        # If user_id parameter is included but not in database, redirect.
-        if not utils.is_user_in_db(user_id):
-            flash("No user found with id {}.".format(user_id))
-            return redirect("/") 
-        print("Using user_id {} for recs.".format(user_id))
-        
-
-    # Login parameter takes precedence.
-    if login:
-        if User.query.filter_by(login=login).count() == 0:
-            flash("No user found with login {}.".format(login))
-            return redirect("/")
-        user_id = User.query.filter_by(login=login).first().user_id
-        print("Using login {} for user_id {} for recs."
-              .format(login, user_id))
-    elif not user_id:
-        user_id = session["user_id"]
-        print("Using logged in user {} for recs.".format(user_id))
-
-    suggestions = rec.get_repo_suggestions(user_id)[offset:limit]
-    repos_query = Repo.query.filter(Repo.repo_id.in_(suggestions),
-                                    Repo.owner_id != user_id)
-    repos = repos_query.all()
-
-    return render_template("repo_recs.html",
-                           repos=repos)
 
 
 @app.route("/recs_react", methods=['GET'])
