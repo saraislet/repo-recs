9c9c649d67935141d80977a0dba538798fa6c058
Sarai <anna.sarai.rosenberg@gmail.com>
2018-03-05 12:58:18 -0800

    increase default_count

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ REFRESH_UPDATE_USER_REPOS_DAYS = 1
 REFRESH_CRAWL_DAYS = 30
 
 # search defaults
-DEFAULT_COUNT = 12
+DEFAULT_COUNT = 15
 
 # URLs, hostnames, and endpoints
 DB_URI = "postgresql:///git_data"
