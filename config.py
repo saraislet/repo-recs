6a72efbbdfbfdc2bd5bafb0062d1ca39850a2895
Sarai <anna.sarai.rosenberg@gmail.com>
2018-03-13 14:21:03 -0700

    adjust count and refresh

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # crawl and refresh config
-MAX_CRAWL_COUNT_TOTAL = 30
-MAX_CRAWL_COUNT_NEW = 30
-REFRESH_UPDATE_REPO_DAYS = 7
-REFRESH_UPDATE_USER_DAYS = 7
-REFRESH_UPDATE_USER_REPOS_DAYS = 1
+MAX_CRAWL_COUNT_TOTAL = 15
+MAX_CRAWL_COUNT_NEW = 15
+REFRESH_UPDATE_REPO_DAYS = 30
+REFRESH_UPDATE_USER_DAYS = 30
+REFRESH_UPDATE_USER_REPOS_DAYS = 3
 REFRESH_CRAWL_DAYS = 30
 
 # search defaults
@@ -12,7 +12,6 @@ DEFAULT_COUNT = 15
 # URLs, hostnames, and endpoints
 DB_URI = "postgresql:///git_data"
 TEST_DB_URI = "postgresql:///git_test"
-AUTH_CALLBACK_URL = "http://127.0.0.1:5000/auth"
 OAUTH_SCOPE = "user user:follow read:user public_repo"
 ENDPOINT = "https://api.github.com"
 GITHUB_AUTH_REQUEST_CODE_URL = "https://github.com/login/oauth/authorize"
