529c56545f742c388873020cc14b4e34bf87e258
Sarai <anna.sarai.rosenberg@gmail.com>
2018-03-18 14:52:11 -0700

    extract updateUser to user.js and implement on home.html

--- static/recs.jsx
+++ static/recs.jsx
@@ -6,55 +6,16 @@ let repoIDs;
 let callsMade = false;
 let scope = document.getElementById("details").dataset.scope;
 
-// window.onload = makeCalls;
-
 ReactDOM.render(
     <PlaceholderList count={12} />,
     document.getElementById("repo-recs")
 );
-
-async function generateCode() {
-    code = Math.random().toString(36).substring(2);
-    return code;
-}
 
 function makeCalls() {
     callsMade = true;
     generateCode()
         .then( thisCode => getRepoRecs(pageNumber, thisCode) )
         .then(updateUser);
-}
-
-function updateUser() {
-    let payload = {method: "POST",
-                   body: JSON.stringify({}),
-                   credentials: "same-origin",
-                   headers: new Headers({"Content-Type": "application/json"})};
-    fetch("/update_user", payload)
-        .then( response => response.json() )
-        .then( (data) => {
-            if (data.Status == 200) {
-                let endUserUpdate = new Date().getTime();
-                var delta = (endUserUpdate - startLoad)/1000;
-                console.log("User updated in " + delta + "seconds.");
-            }})
-        .then( () => crawlUser() );
-}
-
-function crawlUser() {
-    let data = {"crawlFurther": "True"};
-    let payload = {method: "POST",
-                   body: JSON.stringify(data),
-                   credentials: "same-origin",
-                   headers: new Headers({"Content-Type": "application/json"})};
-    fetch("/update_user", payload)
-        .then( response => response.json() )
-        .then( (data) => {
-            if (data.Status == 200) {
-                let endUserCrawl = new Date().getTime();
-                var delta = (endUserCrawl - startLoad)/1000;
-                console.log("User crawled in " + delta + "seconds.");
-            }});
 }
 
 function getRepoRecs(pageNumber, thisCode) {
